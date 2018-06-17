2e2e638f33984fa63932c3b31f46dbfba09745a6
Unknown <bradenmars02@gmail.com>
2018-06-16 22:11:40 -0500

    Admin Control Panel Template, User Control Views
    Template:
    * Bulma-Admin template from (https://dansup.github.io/bulma-templates/templates/admin.html)
    * List Users, Orders, Tracking
    * Postponing for now

--- staff/urls.py
+++ staff/urls.py
@@ -3,7 +3,16 @@ from django.conf import settings
 from django.urls import path
 from . import views
 
-app_name="staff"
+app_name = "staff"
 urlpatterns = [
+
+    # Control Panel
     path('', views.ControlPanel.as_view(), name="control-panel"),
+    path('users/', views.ControlUsers.as_view(), name="control-users"),
+
+    # Users
+    path('users/<int:pk>', views.ViewUser.as_view(), name="view-user"),
+
+    # Tracker
+    path('users/tracker/<int:pk>', views.ViewTracker.as_view(), name="view-tracker"),
 ]
--- staff/views.py
+++ staff/views.py
@@ -3,13 +3,36 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.views.generic import View, TemplateView
 from django.shortcuts import HttpResponse
 from store.models import Cart
+from billing.models import Order
+from billing.models import Invoice
+from tracker.models import Tracker, TrackerUpdate
+from django.contrib.auth.models import User
+
 
 class ControlMixin:
-    
+
+    def user_orders(self, user):
+        orders = Order.objects.filter(user=user)
+        return orders
+
+    def get_tracker(self, order):
+        tracker = Tracker.objects.get(user=self.request.user, order=order)
+        return tracker
+
     @property
-    def orders(self):
+    def carts(self):
         all_carts = Cart.objects.all()
         return all_carts
+
+    @property
+    def users(self):
+        users = User.objects.all()
+        return users
+
+    @property
+    def orders(self):
+        orders = Order.objects.all()
+        return orders
 
 
 class ControlPanel(TemplateView, ControlMixin):
@@ -17,6 +40,38 @@ class ControlPanel(TemplateView, ControlMixin):
 
     def get_context_data(self, **kwargs):
         context = super(ControlPanel, self).get_context_data(**kwargs)
+        context['orders'] = self.carts
+        context['users'] = self.users
+        return context
+
+
+class ControlUsers(TemplateView, ControlMixin):
+    template_name = "staff/users.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ControlUsers, self).get_context_data(**kwargs)
+        context['users'] = self.users
         context['orders'] = self.orders
         return context
-    
+
+
+class ViewUser(TemplateView, ControlMixin):
+    template_name = 'staff/users/view.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ViewUser, self).get_context_data(**kwargs)
+        pk = self.kwargs.get('pk')
+        user = User.objects.get(pk=pk)
+        context['user'] = user
+        context['orders'] = self.user_orders(user)
+        return context
+
+
+class ViewTracker(TemplateView, ControlMixin):
+    template_name = 'staff/users/view_tracker.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ViewTracker, self).get_context_data(**kwargs)
+        order = Order.objects.get(pk=self.kwargs.get('pk'))
+        context['tracker'] = self.get_tracker(order)
+        return context
