b5988549a7953f9ef8db1f54aad6d30a39f4fcad
Braden Mars <bradenmars02@gmail.com>
2018-06-24 19:01:40 -0500

    User Payments View and Template
    Basic view and layout of payments page

--- users/urls.py
+++ users/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     # Pages
     path('', views.AccountPage.as_view(), name="my_account"),
     path('settings/', views.SettingsPage.as_view(), name="my_settings"),
+    path('payments/', views.PaymentsPage.as_view(), name="my_payments"),
     path('invoices/', views.ListInvoices.as_view(), name="my_invoices"),
     path('orders/', views.ListOrders.as_view(), name="my_orders"),
 
-    # Functions
     # Order Search
     path('orders/search/', views.SearchOrders.as_view(), name="search_orders"),
     # Change Phone
