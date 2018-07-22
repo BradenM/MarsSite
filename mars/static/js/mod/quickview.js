87e744b4e2f152a0a699fa1a53030dcd9ccbc9f1
Braden Mars <bradenmars02@gmail.com>
2018-07-22 17:36:15 -0500

    Final Script Fixes
    Final small script fixes before merge

--- mars/static/js/mod/quickview.js
+++ mars/static/js/mod/quickview.js
@@ -1,54 +1,56 @@
-// Quick View
-function closest(el, selector) {
-    var matchesFn;
+$(document).ready(function () {
+    // Quick View
+    function closest(el, selector) {
+        var matchesFn;
 
-    // find vendor prefix
-    ['matches', 'webkitMatchesSelector', 'mozMatchesSelector', 'msMatchesSelector', 'oMatchesSelector'].some(function (fn) {
-        if (typeof document.body[fn] == 'function') {
-            matchesFn = fn;
-            return true;
+        // find vendor prefix
+        ['matches', 'webkitMatchesSelector', 'mozMatchesSelector', 'msMatchesSelector', 'oMatchesSelector'].some(function (fn) {
+            if (typeof document.body[fn] == 'function') {
+                matchesFn = fn;
+                return true;
+            }
+            return false;
+        });
+
+        var parent;
+
+        // traverse parents
+        while (el) {
+            parent = el.parentElement;
+            if (parent && parent[matchesFn](selector)) {
+                return parent;
+            }
+            el = parent;
         }
-        return false;
-    });
 
-    var parent;
-
-    // traverse parents
-    while (el) {
-        parent = el.parentElement;
-        if (parent && parent[matchesFn](selector)) {
-            return parent;
-        }
-        el = parent;
+        return null;
     }
 
-    return null;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Get all document sliders
-    var showQuickview = document.querySelectorAll('[data-show="quickview"]');
-    [].forEach.call(showQuickview, function (show) {
-        var quickview = document.getElementById(show.dataset['target']);
-        if (quickview) {
-            // Add event listener to update output when slider value change
-            show.addEventListener('click', function (event) {
-                quickview.classList.add('is-active');
-            });
-        }
+    document.addEventListener('DOMContentLoaded', function () {
+        // Get all document sliders
+        var showQuickview = document.querySelectorAll('[data-show="quickview"]');
+        [].forEach.call(showQuickview, function (show) {
+            var quickview = document.getElementById(show.dataset['target']);
+            if (quickview) {
+                // Add event listener to update output when slider value change
+                show.addEventListener('click', function (event) {
+                    quickview.classList.add('is-active');
+                });
+            }
+        });
     });
-});
 
-$('.quickview').on("mouseenter", function (event) {
-    // Get all document sliders
-    var dismissQuickView = document.querySelectorAll('[data-dismiss="quickview"]');
-    [].forEach.call(dismissQuickView, function (dismiss) {
-        var quickview = closest(dismiss, '.quickview');
-        if (quickview) {
-            // Add event listener to update output when slider value change
-            dismiss.addEventListener('click', function (event) {
-                quickview.classList.remove('is-active');
-            });
-        }
-    });
+    $('.quickview').on("mouseenter", function (event) {
+        // Get all document sliders
+        var dismissQuickView = document.querySelectorAll('[data-dismiss="quickview"]');
+        [].forEach.call(dismissQuickView, function (dismiss) {
+            var quickview = closest(dismiss, '.quickview');
+            if (quickview) {
+                // Add event listener to update output when slider value change
+                dismiss.addEventListener('click', function (event) {
+                    quickview.classList.remove('is-active');
+                });
+            }
+        });
+    })
 })
